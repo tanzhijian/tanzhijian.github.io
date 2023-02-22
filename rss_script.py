420f2eaaa877186cedddce1c225864ebde9c80de
tanzhijian <i@tanzhijian.org>
2023-02-22 18:54:10 +0800

    排序

--- rss_script.py
+++ rss_script.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os.path
+import time
 
 import markdown
 from jinja2 import Template
@@ -12,19 +14,23 @@ feed = Path(Path.cwd(), "atom.xml")
 def read(path):
     with open(path) as f:
         text = f.read()
-    return text
+    timestamp = os.path.getmtime(path)
+    return text, timestamp
 
 
-def convert(md):
+def convert(md, timestamp):
     html = markdown.markdown(md)
     return {
+        'id': timestamp,
         "html": html,
+        'time': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
     }
 
 
 def export(data, template, feed):
     with open(template) as f:
         template = f.read()
+
     atom = Template(template).render(entries=data)
     with open(feed, "w") as f:
         f.write(atom)
@@ -33,8 +39,12 @@ def export(data, template, feed):
 def main():
     data = []
     for path in path_list:
-        md = read(path)
-        data.append(convert(md))
+        md, timestamp = read(path)
+        data.append(convert(md, timestamp))
+
+    # 这里排序，且只生成十篇文章
+    data = sorted(data, key=lambda x: x['id'], reverse=True)[:10]
+
     export(data, template, feed)
 
 
