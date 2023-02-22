f76a8cf7c276db4ac1e05826becc2ba1f9f57fb5
tanzhijian <i@tanzhijian.org>
2023-02-22 21:44:25 +0800

    完成了生成 atom.xml 脚本

--- rss_script.py
+++ rss_script.py
@@ -11,26 +11,35 @@ template = Path(Path.cwd(), "template.xml")
 feed = Path(Path.cwd(), "atom.xml")
 
 
+def get_summary(text_list):
+    for text in text_list:
+        if text != '\n' and text[0] != '#':
+            return text
+    return ''
+
+
 def read(path):
     with open(path) as f:
-        text = f.read()
+        text_list = f.readlines()
     timestamp = os.path.getmtime(path)
-    return text, timestamp
+    return text_list, timestamp, path.name
 
 
-def convert(md, timestamp):
-    html = markdown.markdown(md)
+def convert(md_list, timestamp, name):
+    html = markdown.markdown(''.join(md_list))
     return {
-        'id': timestamp,
+        "id": timestamp,
+        "title": md_list[0].lstrip('# ').rstrip('\n'),
+        "url": f'https://tanzhijian.org/posts/{name}'.rstrip('.md'),
+        'summary': get_summary(md_list),
         "html": html,
-        'time': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
+        "time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp)),
     }
 
 
 def export(data, template, feed):
     with open(template) as f:
         template = f.read()
-
     atom = Template(template).render(entries=data)
     with open(feed, "w") as f:
         f.write(atom)
@@ -39,11 +48,11 @@ def export(data, template, feed):
 def main():
     data = []
     for path in path_list:
-        md, timestamp = read(path)
-        data.append(convert(md, timestamp))
+        md_list, timestamp, name = read(path)
+        data.append(convert(md_list, timestamp, name))
 
     # 这里排序，且只生成十篇文章
-    data = sorted(data, key=lambda x: x['id'], reverse=True)[:10]
+    data = sorted(data, key=lambda x: x["id"], reverse=True)[:10]
 
     export(data, template, feed)
 
